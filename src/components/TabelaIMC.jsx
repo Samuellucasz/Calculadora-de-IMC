9afad2f70544aaf6b18b073affd72c0c2f55df31
Samuel Lucas <samulucassantosoliveira@gmail.com>
2026-08-14 11:54:32 -0300

    feat: adiciona tabela de classificação do IMC

--- src/components/TabelaIMC.jsx
+++ src/components/TabelaIMC.jsx
@@ -1,7 +1,12 @@
 function TabelaIMC() {
   return (
     <div>
-      Tabela do IMC aparecerá aqui
+      <h2>Classificação do IMC</h2>
+
+      <p>Abaixo de 18,5: Abaixo do peso</p>
+      <p>De 18,5 até 24,9: Peso normal</p>
+      <p>De 25 até 29,9: Sobrepeso</p>
+      <p>30 ou mais: Obesidade</p>
     </div>
   )
 }
